f55394cd00ede1a47b795d7be4dfec6f84b5df73
Alvin <59495152+alvinliauu@users.noreply.github.com>
2020-09-26 21:39:10 +0700

    Add files via upload

---
+++ app/Http/Controllers/RegistersController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Register;
+use Illuminate\Http\Request;
+
+class RegistersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $registers = Register::all();
+        return view('register', ['registers' => $registers]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // $request -> validate([
+        //     'name' => 'required|min:5|max:30'
+        // ]);
+        // dd(request()->all());
+        Register::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password,
+            'gender' => $request->gender,
+            'dateofbirth' => $request->datebirth,
+            'address' => $request->address
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Register $register)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Register $register)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Register $register)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Register $register)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_09_26_033539_create_registers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',30);
+            $table->string('email',255)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',15);
+            $table->string('gender');
+            $table->date('dateofbirth');
+            $table->string('address',255);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registers');
+    }
+}
